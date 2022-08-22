c570edc5eca74974d97799659e27fb8956a92ff9
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-08-22 18:09:30 +0200

    fix

--- src/components/Chambre/index.js
+++ src/components/Chambre/index.js
@@ -2,6 +2,7 @@ export default function Chambre () {
     return (
         <div className="nav_chambre">
             <h1 className="nav_title">Projets Chambre</h1>
+            <h2>salut</h2>
             </div>
     )
 }
