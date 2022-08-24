2c726daf6df5a6d9ead3807130a48fd5d6a9ea1e
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-08-24 12:17:11 +0200

    Some examples for room display

--- src/components/Chambre/index.js
+++ src/components/Chambre/index.js
@@ -1,9 +1,17 @@
+import './styles.scss';
+
 export default function Chambre () {
     return (
         <div className="nav_chambre">
             <h1 className="nav_title">Projets Chambre</h1>
-            <h2>salut</h2>
-            <h2>salut</h2>
+            <div className="nav_chambre_content">
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            <span className="image"><img className="image_bureau" src="https://zupimages.net/up/22/34/ovft.jpg" alt="Logo ArtisenBois" /></span>
+            </div>
             </div>
     )
 }
