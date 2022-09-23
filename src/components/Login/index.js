d276ff9d54d0ced83a5a10298bd029b6cab3ca6c
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-09-23 21:12:30 +0200

    seeding and trying consume the data

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,32 +1,38 @@
 import './styles.scss';
 import { useState, useEffect } from 'react';
 
- export default function Login () {
-    function loadActivity() {
-        fetch("https://api.unsplash.com/photos/?client_id=OmTR2D0ODw2hpL2cC9uicLrdFY-Tnds-TZ3Qg3SVpTk")
-            .then((response) => response.json())
-            .then((data) => {
-                //setimage(data);
-                 console.log(data);
-            });
-            }
+ export default function Login (results) {
+    // function loadActivity() {
+    //     fetch("https://api.unsplash.com/photos/?client_id=OmTR2D0ODw2hpL2cC9uicLrdFY-Tnds-TZ3Qg3SVpTk")
+    //         .then((response) => response.json())
+    //         .then((data) => {
+    //             //setimage(data);
+    //              console.log(data);
+    //         });
+    //         }
     
-    const [image, setimage] = useState([]);
-    useEffect(() => {
-        loadActivity();
-    },  []);
+    // const [image, setimage] = useState([]);
+    // useEffect(() => {
+    //     loadActivity();
+    // },  []);
 
-    function ShowImage() {
-        return (
-            <div>
-                <img src={image.urls.regular} alt={image.alt_description} />
-            </div>
-        )
-    }
+    // function ShowImage() {
+    //     return (
+    //         <div>
+    //             <img src={image.urls.regular} alt={image.alt_description} />
+    //         </div>
+    //     )
+    // }
 
     return (
 <div>
-       <h1>{image}</h1>
+       <ul class="tryout">
+        {results.results.map((result) => (
+            <li key={result.id}>
+                <p>{result.title}</p>
+            </li>
+        ))}
+       </ul>
        </div>
     )
 }
