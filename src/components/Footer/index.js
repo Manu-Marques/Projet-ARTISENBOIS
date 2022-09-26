374df4d0bdd384b3b7a166e086bf962b850f8dcb
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-09-26 18:48:07 +0200

    fix class to className

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -6,20 +6,20 @@ export default function Footer () {
     return (
         <div className="footer-basic">
         <footer>
-        <div class="social"><a href="#">
-            <i class="fa fa-facebook"></i></a><a href="#">
-            <i class='fa fa-twitter'></i></a><a href="#">
-            <i class="fa fa-snapchat"></i></a><a href="#">
-            <i class="fa fa-instagram"></i></a>
+        <div className="social"><a href="#">
+            <i className="fa fa-facebook"></i></a><a href="#">
+            <i className='fa fa-twitter'></i></a><a href="#">
+            <i className="fa fa-snapchat"></i></a><a href="#">
+            <i className="fa fa-instagram"></i></a>
                 </div>
-        <ul class="list-inline">
-            <li class="list-inline-item"><a href="#">Home</a></li>
-            <li class="list-inline-item"><a href="#">Services</a></li>
-            <li class="list-inline-item"><a href="#">About</a></li>
-            <li class="list-inline-item"><a href="#">Terms</a></li>
-            <li class="list-inline-item"><a href="#">Privacy Policy</a></li>
+        <ul className="list-inline">
+            <li className="list-inline-item"><a href="#">Home</a></li>
+            <li className="list-inline-item"><a href="#">Services</a></li>
+            <li className="list-inline-item"><a href="#">About</a></li>
+            <li className="list-inline-item"><a href="#">Terms</a></li>
+            <li className="list-inline-item"><a href="#">Privacy Policy</a></li>
         </ul>
-        <p class="copyright">Company Name © 2018</p>
+        <p className="copyright">Company Name © 2018</p>
     </footer>
 </div>
     )
