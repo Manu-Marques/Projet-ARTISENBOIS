3b1d4008daf5d32950ad4a0c76e3cafcd9f0f4c7
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-09-29 18:10:39 +0200

    Adding and fixing some features

--- src/components/Chambres/index.js
+++ src/components/Chambres/index.js
@@ -1,17 +1,19 @@
 import './styles.scss';
 import { NavLink } from 'react-router-dom';
+import Bureau from '../../../src/assets/bureau.jpg';
 
-export default function Chambres(results) {
+
+export default function Chambre(results) {
+
   return (
     <div className="main">
-      <h1>Chambres</h1>
+      <h1>Chambre</h1>
       <div className="row1">
-
         {
           results.results?.map((result) => {
             return (
               <div key={result.id} className="image">
-                <img src="https://zupimages.net/up/22/35/f2n4.jpg" alt="" />
+                <img src={Bureau} alt="" />
                 <div className="details">
                   <h2>{result.title}</h2>
                   <p>Le lorem ipsum est, en imprimerie, une suite de mots sans signification utilisée à titre provisoire pour calibrer une mise en page.</p>
