5a833825816693b6b3f031a2c7c5d57bd09fcc39
Emanuel Marques Le <le.marques.emanuel@gmail.com>
2022-10-12 15:06:11 +0200

    change for @media on header

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,3 +1,24 @@
 import './styles.scss';
 import { NavLink } from 'react-router-dom';
 
+
+export default function Login() {
+    return (
+        <><section class="top-nav">
+            <div>
+                Logo Here
+            </div>
+            <input id="menu-toggle" type="checkbox" />
+            <label class='menu-button-container' for="menu-toggle">
+                <div class='menu-button'></div>
+            </label>
+            <ul class="menu">
+                <li>One</li>
+                <li>Two</li>
+                <li>Three</li>
+                <li>Four</li>
+                <li>Five</li>
+            </ul>
+        </section><h2>Resize window to collapse menu</h2></>
+    )
+}
