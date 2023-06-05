a190a7899d4cd73e28a5ceb20d5d79187f95c68f
Manu <le.marques.emanuel@gmail.com>
2023-06-05 10:55:30 +0200

    adding data

---
+++ src/data/revetements-sols.js
@@ -0,0 +1,58 @@
+[
+    {
+        "Parquets": [
+            {
+                "id": 1,
+                "title": "Parquet en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Parquet en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Escaliers": [
+            {
+                "id": 1,
+                "title": "Escalier en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Escalier en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ]
+    }
+]
