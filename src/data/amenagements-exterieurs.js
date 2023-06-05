96aa6c958705edb84d063d41613fe57ad8d12055
Manu <le.marques.emanuel@gmail.com>
2023-06-05 10:46:19 +0200

    adding data

--- src/data/amenagements-exterieurs.js
+++ src/data/amenagements-exterieurs.js
@@ -1,61 +1,9 @@
 [
     {
-        "Porte d'entrée": [
+        "Terrasses en bois": [
             {
                 "id": 1,
-                "title": "Porte en bois",
-                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem",
-                "rating": 4.5,
-                "type": "Chêne",
-                "dimensions": "150x100x50",
-                "weight": "80kg",
-                "color": "Marron",
-                "doors": "2",
-                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-            },
-            {
-                "id": 2,
-                "title": "Porte en PVC",
-                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
-                "rating": 4,
-                "type": "Chêne",
-                "dimensions": "200x100x50",
-                "weight": "100kg",
-                "color": "Marron",
-                "doors": "2",
-                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-            },
-        ],
-        "Porte de garage": [
-            {
-                "id": 1,
-                "title": "Porte en métal",
-                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
-                "rating": 4,
-                "type": "Chêne",
-                "dimensions": "200x100x50",
-                "weight": "100kg",
-                "color": "Marron",
-                "doors": "2",
-                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-            },
-            {
-                "id": 2,
-                "title": "Porte en plastique",
-                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
-                "rating": 4,
-                "type": "Chêne",
-                "dimensions": "200x100x50",
-                "weight": "100kg",
-                "color": "Marron",
-                "doors": "2",
-                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-            }
-        ],
-        "Fenêtres et portes fenêtres": [
-            {
-                "id": 1,
-                "title": "Fenêtre en bois",
+                "title": "Terrasse chêne",
                 "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
                 "rating": 4.5,
                 "type": "Chêne",
@@ -67,7 +15,7 @@
             },
             {
                 "id": 2,
-                "title": "Porte fenêtre en PVC",
+                "title": "Terrasse en PVC",
                 "description": "à venir",
                 "price": 100,
                 "rating": 4.5,
@@ -79,62 +27,58 @@
                 "image": "https://zupimages.net/up/22/40/lhhm.jpg"
             }
         ],
-        "Volets": [
+        "Terrasses composite": [
             {
-                "volets roulants": [
-                    {
-                        "id": 1,
-                        "title": "Volet roulant en bois",
-                        "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
-                        "rating": 4.5,
-                        "type": "Chêne",
-                        "dimensions": "200x100x50",
-                        "weight": "100kg",
-                        "color": "Marron",
-                        "doors": "2",
-                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-                    },
-                    {
-                        "id": 2,
-                        "title": "Volet roulant en PVC",
-                        "description": "à venir",
-                        "price": 100,
-                        "rating": 4.5,
-                        "type": "Chêne",
-                        "dimensions": "200x100x50",
-                        "weight": "100kg",
-                        "color": "Marron",
-                        "doors": "2",
-                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-                    }
-                ],
-                "volets battants": [
-                    {
-                        "id": 1,
-                        "title": "Volet battant en bois",
-                        "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
-                        "rating": 4.5,
-                        "type": "Chêne",
-                        "dimensions": "200x100x50",
-                        "weight": "100kg",
-                        "color": "Marron",
-                        "doors": "2",
-                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-                    },
-                    {
-                        "id": 2,
-                        "title": "Volet battant en PVC",
-                        "description": "à venir",
-                        "price": 100,
-                        "rating": 4.5,
-                        "type": "Chêne",
-                        "dimensions": "200x100x50",
-                        "weight": "100kg",
-                        "color": "Marron",
-                        "doors": "2",
-                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
-                    }
-                ]
+                "id": 1,
+                "title": "Terrasse composite",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Terrasse composite",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Autres": [
+            {
+                "id": 1,
+                "title": "Autre",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Autre",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
             }
         ]
     }
---
+++ src/data/menuiserie-exterieurs.js
@@ -0,0 +1,141 @@
+[
+    {
+        "Porte d'entrée": [
+            {
+                "id": 1,
+                "title": "Porte en bois",
+                "description": "Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "150x100x50",
+                "weight": "80kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Porte en PVC",
+                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+        ],
+        "Porte de garage": [
+            {
+                "id": 1,
+                "title": "Porte en métal",
+                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Porte en plastique",
+                "description": "Lorem ipsum dolor sit amlit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Fenêtres et portes fenêtres": [
+            {
+                "id": 1,
+                "title": "Fenêtre en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": 2,
+                "title": "Porte fenêtre en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Volets": [
+            {
+                "volets roulants": [
+                    {
+                        "id": 1,
+                        "title": "Volet roulant en bois",
+                        "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                        "rating": 4.5,
+                        "type": "Chêne",
+                        "dimensions": "200x100x50",
+                        "weight": "100kg",
+                        "color": "Marron",
+                        "doors": "2",
+                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+                    },
+                    {
+                        "id": 2,
+                        "title": "Volet roulant en PVC",
+                        "description": "à venir",
+                        "price": 100,
+                        "rating": 4.5,
+                        "type": "Chêne",
+                        "dimensions": "200x100x50",
+                        "weight": "100kg",
+                        "color": "Marron",
+                        "doors": "2",
+                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+                    }
+                ],
+                "volets battants": [
+                    {
+                        "id": 1,
+                        "title": "Volet battant en bois",
+                        "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                        "rating": 4.5,
+                        "type": "Chêne",
+                        "dimensions": "200x100x50",
+                        "weight": "100kg",
+                        "color": "Marron",
+                        "doors": "2",
+                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+                    },
+                    {
+                        "id": 2,
+                        "title": "Volet battant en PVC",
+                        "description": "à venir",
+                        "price": 100,
+                        "rating": 4.5,
+                        "type": "Chêne",
+                        "dimensions": "200x100x50",
+                        "weight": "100kg",
+                        "color": "Marron",
+                        "doors": "2",
+                        "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+                    }
+                ]
+            }
+        ]
+    }
+]
