e3ef92a4ca8e59734e09c5b4ef4810d4fd8e0016
Manu <le.marques.emanuel@gmail.com>
2023-06-05 10:52:40 +0200

    adding data

---
+++ src/data/agencements-sur-mesure.js
@@ -0,0 +1,139 @@
+[
+    {
+        "Sous escaliers": [
+            {
+                "id": "1",
+                "title": "Sous escaliers en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": "2",
+                "title": "Sous escaliers en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Sous pentes": [
+            {
+                "id": "1",
+                "title": "Sous pentes en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": "2",
+                "title": "Sous pentes en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Placards sur mesure": [
+            {
+                "id": "1",
+                "title": "Placards sur mesure en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": "2",
+                "title": "Placards sur mesure en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Cuisine sur mesure": [
+            {
+                "id": "1",
+                "title": "Cuisine sur mesure en bois",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": "2",
+                "title": "Cuisine sur mesure en PVC",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ],
+        "Autres": [
+            {
+                "id": "1",
+                "title": "Autre",
+                "description": "Lorem ipsum d consectetur adipisicing elit. Voluptatum earum nesciunt nulla veritatis natus ratione repellat dolorum, quidem, enim voluptatem at ullam nihil providen non facilis fugit consequatur, fuga alias!",
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            },
+            {
+                "id": "2",
+                "title": "Autre",
+                "description": "à venir",
+                "price": 100,
+                "rating": 4.5,
+                "type": "Chêne",
+                "dimensions": "200x100x50",
+                "weight": "100kg",
+                "color": "Marron",
+                "doors": "2",
+                "image": "https://zupimages.net/up/22/40/lhhm.jpg"
+            }
+        ]
+    }
+]
